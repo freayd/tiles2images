30051b627486a2019f404ca7ca2fe9944611bd08
freayd <freayd@users.noreply.github.com>
2015-11-12 18:52:19 +1000

    Generates zoom range

--- tiles2images.rb
+++ tiles2images.rb
@@ -3,47 +3,49 @@ require 'net/http'
 require 'rmagick'
 require 'uri'
 
-zoom = 2
 dir = 'osm'
 url = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-list = Magick::ImageList.new
 extension = File.extname(url)
-(2 ** zoom).times do |x|
-  paths = (2 ** zoom).times.map do |y|
-    path = File.join(__dir__, dir, zoom.to_s, x.to_s, "#{y}#{extension}")
+(0..4).each do |zoom|
+  list = Magick::ImageList.new
+  (2 ** zoom).times do |x|
+    paths = (2 ** zoom).times.map do |y|
+      path = File.join(__dir__, dir, zoom.to_s, x.to_s, "#{y}#{extension}")
 
-    unless File.exist?(path)
-      FileUtils.makedirs(File.dirname(path))
-      uri = URI(url.sub('{z}', zoom.to_s).sub('{x}', x.to_s).sub('{y}', y.to_s))
-      sleep 0.1
-      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.port == 443) do |http|
-        begin
-          File.open(path, 'wb') do |file|
-            http.request_get(uri.request_uri) do |response|
-              response.read_body do |segment|
-                file.write(segment)
+      unless File.exist?(path)
+        FileUtils.makedirs(File.dirname(path))
+        uri = URI(url.sub('{z}', zoom.to_s).sub('{x}', x.to_s).sub('{y}', y.to_s))
+        sleep 0.1
+        Net::HTTP.start(uri.host, uri.port, use_ssl: uri.port == 443) do |http|
+          begin
+            File.open(path, 'wb') do |file|
+              http.request_get(uri.request_uri) do |response|
+                response.read_body do |segment|
+                  file.write(segment)
+                end
               end
             end
+          rescue Exception
+            File.delete(path)
+            raise
           end
-        rescue Exception
-          File.delete(path)
-          raise
         end
       end
+
+      path
     end
-
-    path
+    list << Magick::ImageList.new(*paths).append(true)
   end
-  list << Magick::ImageList.new(*paths).append(true)
-end
-path = File.join(__dir__, dir, zoom.to_s)
-image = list.append(false)
-image.write("#{path}#{extension}") unless File.exists?("#{path}#{extension}")
+  path = File.join(__dir__, dir, zoom.to_s)
+  image = list.append(false)
 
-unless File.exist?("#{path}.tiff")
-  image.write("#{path}.tmp.tiff")
-  gdal_options = "-of GTiff -a_srs EPSG:3857 -a_ullr -20037508.34 20037508.34 20037508.34 -20037508.34"
-  system("gdal_translate #{gdal_options} '#{path}.tmp.tiff' '#{path}.tiff'")
-  File.delete("#{path}.tmp.tiff")
+  image.write("#{path}#{extension}") unless File.exists?("#{path}#{extension}")
+
+  unless File.exist?("#{path}.tiff")
+    image.write("#{path}.tmp.tiff")
+    gdal_options = "-of GTiff -a_srs EPSG:3857 -a_ullr -20037508.34 20037508.34 20037508.34 -20037508.34"
+    system("gdal_translate #{gdal_options} '#{path}.tmp.tiff' '#{path}.tiff'")
+    File.delete("#{path}.tmp.tiff")
+  end
 end
